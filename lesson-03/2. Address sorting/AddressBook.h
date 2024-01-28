fe57aed311ca23f7626a284871c066f1123dcbfa
Alexander Eismont <alex.v.eismont@gmail.com>
2024-01-28 22:00:52 +0300

    Added second task

---
+++ lesson-03/2. Address sorting/AddressBook.h
@@ -0,0 +1,23 @@
+#ifndef ADDRESS_BOOK_H
+#define ADDRESS_BOOK_H
+
+#include <vector>
+#include "Address.h"
+
+class AddressBook
+{
+private:
+	int m_size;
+	std::vector<Address> m_addresses;
+
+public:
+	AddressBook(int size, const std::vector<Address>& address);
+
+	void readFromFile(const std::string& filename);
+
+	void sortAddressesByCity();
+
+	void writeToFile(const std::string& filename);
+};
+
+#endif
