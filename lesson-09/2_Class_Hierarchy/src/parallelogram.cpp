975fc3dba6be73ab6c7ad9acba1ba123d8b7f931
Alexander Eismont <alex.v.eismont@gmail.com>
2024-02-20 20:49:36 +0300

    Added second task

---
+++ lesson-09/2_Class_Hierarchy/src/parallelogram.cpp
@@ -0,0 +1,9 @@
+#include "parallelogram.h"
+
+Parallelogram::Parallelogram()
+    : Quadrangle()
+{
+    m_figure_name = "Parallelogram";
+    m_sides = { 2, 3, 2, 3 };
+    m_angles = { 60, 120, 60, 120 };
+}
---
+++ lesson-09/2_Class_Hierarchy/src/rhombus.h
@@ -0,0 +1,12 @@
+#ifndef RHOMBUS_H
+#define RHOMBUS_H
+
+#include "parallelogram.h"
+
+class Rhombus : public Parallelogram
+{
+public:
+    Rhombus();
+};
+
+#endif
